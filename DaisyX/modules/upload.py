1a729a5100cce6e56c86eb3e1fffaa80c36f73c4
omira geevin <77577832+omiragk@users.noreply.github.com>
2021-04-16 19:28:41 +0530

    Update upload.py

--- DaisyX/modules/upload.py
+++ DaisyX/modules/upload.py
@@ -83,7 +83,7 @@ async def tsh(event):
 
             str(time.time() - start)
             await orta.edit(
-                f"File Successfully Uploaded to TransferSh.\n\nLink 👉 {download_link}\nExpired Date 👉 {final_date}\n\nUploaded by Anna 👸"
+                f"File Successfully Uploaded to TransferSh.\n\nLink 👉 {download_link}\nExpired Date 👉 {final_date}\n\nUploaded by @MissAnna_robot 👸"
             )
         except Exception as e:
             traceback.print_exc()
@@ -118,7 +118,7 @@ async def tmp(event):
 
             str(time.time() - start)
             await orta.edit(
-                f"File Successfully Uploaded to TmpNinja.\n\nLink 👉 {download_link}\n\nUploaded by Anna 👸"
+                f"File Successfully Uploaded to TmpNinja.\n\nLink 👉 {download_link}\n\nUploaded by @MissAnna_robot 👸"
             )
         except Exception as e:
             traceback.print_exc()
@@ -159,7 +159,7 @@ async def up(event):
                 event.chat.id,
                 dosya,
                 force_document=True,
-                caption=f"Uploaded By Anna",
+                caption=f"Uploaded By @MissAnna_robot ",
             )
         except Exception as e:
             traceback.print_exc()
@@ -182,7 +182,6 @@ if __name__ == "__main__":
 
 __help__ = """
  ⦁ `/transfersh`*:* reply to a telegram file to upload it on transfersh and get direct download link
-
 
 © @omiragk
 """
